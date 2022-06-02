fe5a0b09015ed3e53d67740c4e2a9b1f2bb95cc4
Huongfarm <106758058+Huongfarm@users.noreply.github.com>
2022-06-02 22:22:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "heloo",
-itrotitle:'Chu Văn Đạt ',
+introTitle: 'Chu Văn Đạt',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
